d7790b53115e9f62550e3c77a46ab7123033d259
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-09-07 17:05:42 +0700

    feat: Add '1. Defining a module' and '2. Reavealing module pattern' sections

---
+++ javascript/factory-functions-and-the-module-pattern/practice/module-pattern.js
@@ -0,0 +1,68 @@
+// 1.Defining a module
+(function() {
+
+  // declare private variables and/or functions
+
+  return {
+      // declare public variables and/or functions
+  }
+
+})();
+
+const HTMLChanger = (() => {
+  const contents = 'contents';
+
+  const changeHTML = () => {
+    const element = document.querySelector('.module-pattern');
+    element.textContent = contents;
+  };
+
+  const writeToDOM = (selector, message) => {
+    const div = document.querySelector(selector);
+
+    div.textContent = message;
+  };
+
+  return {
+    callChangeHTML: function() {
+      changeHTML();
+      console.log(contents);
+    },
+    writeToDOM
+  };
+})();
+
+HTMLChanger.callChangeHTML(); // output: 'contents';
+HTMLChanger.contents; // undefined
+HTMLChanger.writeToDOM('.module-pattern', 'Hello, world!'); // .module-pattern.textcontent = 'Hello, world!';
+
+// 2. Revealing module pattern
+const Exposer = (() => {
+  let privateVariable = 10;
+
+  const privateMethod = () => {
+    console.log("Inside a private method!");
+    privateVariable += 1;
+  };
+
+  const methodToExpose = () => {
+    console.log('This is a method I want to expose!');
+  };
+
+  const otherMethodIWantToExpose = () => {
+    privateMethod();
+  };
+
+  const getPrivateVariable = () => privateVariable;
+
+  return {
+    first: methodToExpose,
+    second: otherMethodIWantToExpose,
+    getPrivateVariable
+  };
+})();
+
+Exposer.first(); // output: This is a method I want to expose!
+Exposer.second(); // output: Inside a private!
+Exposer.getPrivateVariable; // output: 11
+Exposer.methodToExpose; // output: undefined
