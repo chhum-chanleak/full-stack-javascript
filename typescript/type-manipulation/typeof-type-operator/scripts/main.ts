b840b22b34d96b0718fed294e75d29800b03c9eb
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-10-24 12:36:59 +0700

    feat: Complete typeof-type-operator.

--- typescript/type-manipulation/typeof-type-operator/scripts/main.ts
+++ typescript/type-manipulation/typeof-type-operator/scripts/main.ts
@@ -1,1 +1,15 @@
-console.log("Hello, world! typeof type operator");
+// 1. The 'typeof' type operator
+// Prints "string"
+console.log(typeof "Hello, world!");
+
+let s = "Hello";
+let n: typeof s;
+
+type Predicate = (x: unknown) => boolean;
+type K = ReturnType<Predicate>;
+
+function f() {
+  return { x: 10, y: 3 };
+}
+// type P = ReturnType<f>; // No
+type P = ReturnType<typeof f>; // Yes
