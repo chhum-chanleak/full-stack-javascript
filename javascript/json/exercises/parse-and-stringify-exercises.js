5581263e903732e02ed0980e439b789dced1fab4
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-12-05 20:19:34 +0700

    feat: Complete parse-and-stringify-exercises

--- javascript/json/exercises/parse-and-stringify-exercises.js
+++ javascript/json/exercises/parse-and-stringify-exercises.js
@@ -1,4 +1,4 @@
-// JSON.parse()
+// 1. JSON.parse()
 // The JSON.parse() method in JavaScript is used to convert a JSON string into a JavaScript object. Optionally, it allows a reviver function to modify the object’s structure during the parsing process.
 
 // Example:
@@ -32,26 +32,23 @@ const parsedObj2 = JSON.parse(jsonString2);
 
 // Exercise 2: Using a 'reviver' to modify parsed data
 // Parse a JSON string containing dates and use the 'reviver' function to convert date strings into Date objects.
-const meeting = '{ "topic": "climate-change", "date": "05.12.2024"}';
+const meeting = '[{ "topic": "climate-change", "date": "05.12.2024"}, { "topic": "climate-change", "date": "05.12.2024"}]';
 
-const reviver = (arrayOfObjects) => {
-
-  for (let i = 0; i < arrayOfObjects.length; i += 1) {
-    for (const key in arrayOfObjects[i]) {
-      const value = arrayOfObjects[i][key];
-  
-      (key === "date") ? new Date() : value;
-    }
+const reviver = (key, value) => {  
+  if (key === "date") {
+    return new Date();
   }
+
+  return value;
 };
 
-const date = JSON.parse(meeting, reviver(meeting))
+const dates = JSON.parse(meeting, reviver);
 
 // console.log(meeting);
-// console.log(date.date);
+// console.log(dates);
 // reviver(JSON.parse(jsonString2));
 
-// JSON.stringify()
+// 2. JSON.stringify()
 // The JSON.stringify() method converts a JavaScript object or value into a JSON string. It is used to serialize objects for storage, data transfer, or debugging. JSON.stringify() has three parameters such as 'value', 'replacer' and 'space', however, the second and third are optional. 
 
 // 'value' parameter (required):
@@ -64,6 +61,9 @@ const jsonString3 = JSON.stringify(obj);
 
 // Exercise: Convert an Object to JSON String
 // Write a function convertToJSON that takes an object and returns its JSON string using JSON.stringify() with just one parameter. Log the resulting JSON string to the console.
+const convertToJSON = (obj) => JSON.stringify(obj);
+
+// console.log(convertToJSON(obj));
 
 // 'replacer' parameter (optional):
 // A function or array that filters or modifies the properties to be included in the JSON string:
@@ -89,6 +89,22 @@ const jsonString5 = JSON.stringify(product, replacer, 2);
 
 // Exercise: Use JSON.stringify() with a Replacer Function
 // Write a function filterSensitiveData that takes an object and returns a JSON string. Use a replacer function to exclude any properties that have the key password or ssn. The resulting JSON string should not contain these sensitive properties.
+const admins = [{ name: "apple", password: "123", ssn: "321"}, { name: "banana", password: "456", ssn: "654"}];
+
+// Remove 'password' and 'ssn' properties from json string
+const replacer2 = (key, value) => {
+  if (key === "password" || key === "ssn") {
+    return undefined;
+  }
+
+  return value;
+}
+
+const filterSensitiveData = (obj) => JSON.stringify(obj, replacer2);
+const adminsWithNoSensitiveData = filterSensitiveData(admins);
+
+// console.log(admins);
+// console.log(adminsWithNoSensitiveData);
 
 // 'space' parameter (optional):
 // Adds indentation and formatting to the JSON string for readability:
@@ -105,11 +121,23 @@ const user = {
 };
 
 const jsonString6 = JSON.stringify(user, null, 4);
+
 // console.log(jsonString6);
 
 // Exercise 'space' parameter: Format JSON with the 'space' Parameter
 // Write a function called formatPersonData that accepts a person object with properties name, age, location, and contact. The function should return the formatted JSON string with 2 spaces of indentation.
+const user2 = {
+  name: "Coconut",
+  age: 30,
+  loaction: {
+    street: "123 Main St",
+    city: "coconutland",
+    postalCode: "12333"
+  },
+  contact: "999 777 555",  
+};
 
+const formatPersonData = (personObj) => JSON.stringify(personObj, null, 2);
+const personData = formatPersonData(user2);
 
-
-
+// console.log(personData);
