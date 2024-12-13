f1fc871d8d476e2ea63a862f4b24f4f02210aa91
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-12-13 15:09:51 +0700

    feat: Add errors.ts

---
+++ javascript/oop-principles/exercises/errors.ts
@@ -0,0 +1,6 @@
+// Error messages
+export const errorMessages = {
+  MISSING_NAME_SHAPE_PARAMETERS: "name and shape parameters are required",
+  NO_EXISTENCE: "does not exist",
+  EMPTINESS: "is empty",
+};
