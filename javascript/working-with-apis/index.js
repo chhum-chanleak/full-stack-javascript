9654b51ed2ea644b1062d34610935dc97c590618
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-01-23 07:57:51 +0700

    feat: fetch api.gipy

--- javascript/working-with-apis/index.js
+++ javascript/working-with-apis/index.js
@@ -1,11 +1,16 @@
+// fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=Put_Your_Key_Here'
+// )
+// .then(response => console.log(response))
+// .catch(error => console.log(error));
 
-fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=Put_Your_Key_Here'
-)
-.then(result => console.log(result))
-.catch(error => console.log(error));
+// console.log("Hello, world!");
 
-console.log("Hello, world!");
-
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=Put_Your_Key_Here&s=cats')
-.then((result) => console.log(result))
-.catch(error => console.log(error));
+const img = document.querySelector("img");
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=key_value&s=cats')
+.then((response) => response.json())
+.then((response) => {
+  img.src = `${response.data.images.original.url}`;
+  document.body.appendChild(img);
+  console.log(response);
+})
+.catch((error) => console.log(error));
